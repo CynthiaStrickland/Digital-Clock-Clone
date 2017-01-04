a71b25e87207218e689996080e4fb00768a6d091
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-01-04 11:48:08 -0800

    Updates

--- DigitalClockClone/ClockViewController.swift
+++ DigitalClockClone/ClockViewController.swift
@@ -16,21 +16,21 @@ class ClockViewController: UIViewController {
         super.viewDidLoad()
 
       myTimeLabel.text = ""
-      _ = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: ("updateClock"), userInfo: nil, repeats: true)
+      _ = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: (#selector(ClockViewController.updateClock)), userInfo: nil, repeats: true)
     }
 
   func updateClock() {
     
-    let date = NSDate()
-    let calendar = NSCalendar.currentCalendar()
-    let components = calendar.components([.Hour, .Minute, .Second], fromDate: date)
+    let date = Date()
+    let calendar = Calendar.current
+    let components = (calendar as NSCalendar).components([.hour, .minute, .second], from: date)
     
-    var hour = components.hour > 12 ? components.hour - 12 : components.hour
+    var hour = components.hour! > 12 ? components.hour! - 12 : components.hour
     hour = hour == 0 ? 12 : hour
-    let hourString = hour > 9 ? "\(hour)" : "0\(hour)"
-    let minutes = components.minute > 9 ? "\(components.minute)" : "0\(components.minute)"
-    let seconds = components.second > 9 ? "\(components.second)" : "0\(components.second)"
-    let am = components.hour > 12 ? "AM" : "PM"
+    let hourString = hour! > 9 ? "\(hour)" : "0\(hour)"
+    let minutes = components.minute! > 9 ? "\(components.minute)" : "0\(components.minute)"
+    let seconds = components.second! > 9 ? "\(components.second)" : "0\(components.second)"
+    let am = components.hour! > 12 ? "AM" : "PM"
     
     myTimeLabel.text = "\(hourString):\(minutes):\(seconds) \(am)"
   }
